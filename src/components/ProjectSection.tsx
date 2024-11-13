a0712cfb3461b7da4dcdf22ecdb8b099ed3544ff
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2024-11-14 02:02:14 +0700

    style: fix text font bug

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -24,10 +24,10 @@ export function ProjectSection({
       <p className="flex w-fit items-center gap-2 rounded-sm border border-t-opp-medium bg-base px-4 py-2 text-sm font-bold text-t-medium">
         <Sparkles className="size-4 stroke-2" /> Project
       </p>
-      <h2 className="mt-3 bg-gradient-to-r from-accent to-secondary bg-clip-text text-4xl font-extrabold text-transparent lg:text-[54px]">
+      <h2 className="mt-3 bg-gradient-to-r from-accent to-secondary bg-clip-text text-4xl font-extrabold leading-tight text-transparent lg:text-[54px]">
         Check out my featured <br hidden className="lg:block" /> projects
       </h2>
-      <div className="mt-10 grid w-full grid-cols-1 gap-8 leading-tight sm:grid-cols-2 lg:gap-12 2xl:grid-cols-4">
+      <div className="mt-10 grid w-full grid-cols-1 gap-8 sm:grid-cols-2 lg:gap-12 2xl:grid-cols-4">
         <ProjectCard cardDetails={temp} />
         <ProjectCard cardDetails={temp} />
         <ProjectCard cardDetails={temp} />
