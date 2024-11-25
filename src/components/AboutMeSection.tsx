5bcc4aaf25d10b35155e835b24e7e5cf74ed26c6
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2024-11-26 00:48:06 +0700

    feat: update about me section

--- src/components/AboutMeSection.tsx
+++ src/components/AboutMeSection.tsx
@@ -5,6 +5,8 @@ import { Download, Sparkles } from "lucide-react";
 import Link from "next/link";
 import { Tag } from "./ProjectCard";
 import { FadeInAnimation } from "./animation/FadeInAnimation";
+import Image from "next/image";
+import AwardSection from "./AwardSection";
 
 export function AboutMe({
   className = "",
@@ -22,247 +24,53 @@ export function AboutMe({
       </FadeInAnimation>
       <FadeInAnimation index={2}>
         <h2 className="mt-3 bg-gradient-to-r from-accent to-secondary bg-clip-text text-3xl font-extrabold leading-tight text-transparent lg:text-[54px]">
-          Turning complex problems into simple web application
+          Hi, I am Phongit
+          <br />
         </h2>
+        <p className="text-2xl font-bold text-t-bright lg:text-4xl">
+          Turning complex problems into simple web application
+        </p>
       </FadeInAnimation>
-      <div className="py-12">
-        {/* Stats Section */}
-        <div className="mb-10 flex flex-col items-center justify-around gap-8 lg:flex-row">
-          <FadeInAnimation className="w-full rounded-md border-[0.5px] border-t-opp-medium p-6 text-center">
-            <p className="bg-gradient-to-r from-t-accent to-t-secondary bg-clip-text text-5xl font-bold text-transparent">
-              40+
-            </p>
-            <p className="text-lg font-semibold text-t-bright">Happy clients</p>
-          </FadeInAnimation>
 
-          <FadeInAnimation
-            index={2}
-            className="w-full rounded-md border-[0.5px] border-t-opp-medium p-6 text-center"
-          >
-            <p className="bg-gradient-to-r from-t-accent to-t-secondary bg-clip-text text-5xl font-bold text-transparent">
-              2+
-            </p>
-            <p className="text-lg font-semibold text-t-bright">
-              Years of experience
-            </p>
-          </FadeInAnimation>
-
-          <FadeInAnimation
-            index={3}
-            className="w-full rounded-md border-[0.5px] border-t-opp-medium p-6 text-center"
-          >
-            <p className="bg-gradient-to-r from-t-accent to-t-secondary bg-clip-text text-5xl font-bold text-transparent">
-              50+
-            </p>
-            <p className="text-lg font-semibold text-t-bright">Projects done</p>
-          </FadeInAnimation>
+      <FadeInAnimation index={3}>
+        <div className="relative mt-8 flex h-64 w-full items-center gap-4 overflow-x-scroll lg:justify-center lg:overflow-visible">
+          {[
+            { imageUrl: "image1.jpg", left: "-20%", rotate: "4deg", alt: "" },
+            { imageUrl: "image2.jpg", left: "8%", rotate: "-3deg", alt: "" },
+            { imageUrl: "image4.jpg", left: "36%", rotate: "5deg", alt: "" },
+            { imageUrl: "image3.png", left: "64%", rotate: "-2deg", alt: "" },
+            { imageUrl: "image5.jpg", left: "92%", rotate: "5deg", alt: "" },
+          ].map((item, index) => {
+            return (
+              <Image
+                src={`/aboutme/${item.imageUrl}`}
+                alt={`Image`}
+                width={500}
+                height={500}
+                style={{
+                  left: `${item.left}`,
+                  transform: `rotate(${item.rotate})`,
+                }}
+                className="-z-10 aspect-square w-52 rounded-sm object-cover object-center lg:absolute"
+              />
+            );
+          })}
         </div>
+      </FadeInAnimation>
+      <FadeInAnimation index={4}>
+        <AwardSection />
+      </FadeInAnimation>
 
-        {/* Bio Section */}
-        <div className="flex flex-col items-start gap-10 lg:flex-row">
-          {/* Text Section */}
-          <div className="text- lg:w-2/3">
-            <FadeInAnimation>
-              <p className="mb-4 text-t-bright">
-                I wonder if I've been changed in the night? Let me think. Was I
-                the same when I got up this morning? I almost think I can
-                remember feeling a little different. But if I'm not the same,
-                the next question is 'Who in the world am I?' Ah, that's the
-                great puzzle!
-              </p>
-            </FadeInAnimation>
-            <FadeInAnimation index={3}>
-              <p>
-                Be what you would seem to be - or, if you'd like it put more
-                simply - never imagine yourself not to be otherwise than what it
-                might appear to others that what you were or{" "}
-                <u>might have been</u> was not otherwise than what you had been
-                would have appeared to them to be otherwise.
-              </p>
-            </FadeInAnimation>
-          </div>
+      {/* Download CV Button */}
 
-          {/* Contact Section */}
-          <div className="space-y-4 lg:w-1/3">
-            <FadeInAnimation>
-              <h3>
-                <small className="text-sm text-t-bright">Name</small>
-              </h3>
-              <p className="text-lg font-semibold text-t-bright">Alex Walker</p>
-            </FadeInAnimation>
-            <FadeInAnimation index={3}>
-              <h3>
-                <small className="text-sm text-t-bright">Phone</small>
-              </h3>
-              <p className="text-lg font-semibold text-t-bright">
-                +1 212-708-9400
-              </p>
-            </FadeInAnimation>
-            <FadeInAnimation index={5}>
-              <h3>
-                <small className="text-sm text-t-bright">Email</small>
-              </h3>
-              <p className="text-lg font-semibold text-t-bright">
-                hello@yourdomain.com
-              </p>
-            </FadeInAnimation>
-            <FadeInAnimation index={6}>
-              <h3>
-                <small className="text-sm text-t-bright">Location</small>
-              </h3>
-              <p className="text-lg font-semibold text-t-bright">
-                Odesa, Ukraine
-              </p>
-            </FadeInAnimation>
-          </div>
-        </div>
-
-        {/* Download CV Button */}
-
-        <FadeInAnimation className="mt-10 text-center">
-          <Link
-            href="project"
-            className="flex w-full items-center justify-center gap-2 rounded-md bg-gradient-to-r from-accent to-secondary px-5 py-2 text-lg font-bold text-t-opp-bright transition-transform duration-300 hover:scale-105 focus-visible:scale-105 lg:w-fit"
-          >
-            Download Resume <Download className="size-4 stroke-[3px]" />
-          </Link>
-        </FadeInAnimation>
-      </div>
-      <div className="grid grid-cols-1 gap-8 text-t-bright lg:grid-cols-2 lg:py-12">
-        {/* Frontend Development Card */}
-
-        <FadeInAnimation
-          index={1}
-          className="h-[620px] space-y-4 rounded-2xl border border-t-opp-medium p-6"
+      <FadeInAnimation className="mt-4 text-center">
+        <Link
+          href="project"
+          className="flex w-full items-center justify-center gap-2 rounded-md bg-gradient-to-r from-accent to-secondary px-5 py-2 text-lg font-bold text-t-opp-bright transition-transform duration-300 hover:scale-105 focus-visible:scale-105 lg:w-fit"
         >
-          <h3 className="text-2xl font-semibold text-t-bright">
-            Frontend development
-          </h3>
-          <div className="flex space-x-2">
-            <Tag
-              color="white"
-              className="border border-t-opp-medium bg-transparent text-t-bright"
-            >
-              UX/UI Design
-            </Tag>
-            <Tag
-              color="white"
-              className="border border-t-opp-medium bg-transparent text-t-bright"
-            >
-              Design to Code
-            </Tag>
-          </div>
-          <p className="text-t-bright">
-            I work with HTML/CSS, Framer and WordPress.
-          </p>
-          <img
-            src="/path/to/frontend-image.png"
-            alt="Frontend Development"
-            className="w-full rounded-md"
-          />
-        </FadeInAnimation>
-
-        {/* Digital Art and Graphic Design Card */}
-
-        <FadeInAnimation
-          index={3}
-          className="h-[620px] space-y-4 rounded-2xl border border-t-opp-medium p-6"
-        >
-          <h3 className="text-2xl font-semibold text-t-bright">
-            Digital art and graphic design
-          </h3>
-          <div className="flex space-x-2">
-            <Tag
-              color="white"
-              className="border border-t-opp-medium bg-transparent text-t-bright"
-            >
-              Illustrations
-            </Tag>
-            <Tag
-              color="white"
-              className="border border-t-opp-medium bg-transparent text-t-bright"
-            >
-              AI Experiments
-            </Tag>
-          </div>
-          <p className="text-t-bright">
-            I use Adobe Photoshop, Fresco and Blender to create illustrations
-            and 3D renders for my clients and stocks.
-          </p>
-          <img
-            src="/path/to/design-image.png"
-            alt="Graphic Design"
-            className="w-full rounded-md"
-          />
-        </FadeInAnimation>
-
-        {/* SEO/Digital Marketing Solutions Card */}
-
-        <FadeInAnimation
-          index={5}
-          className="h-[620px] space-y-4 rounded-2xl border border-t-opp-medium p-6"
-        >
-          <h3 className="text-2xl font-semibold text-t-bright">
-            SEO/Digital marketing solutions
-          </h3>
-          <div className="flex space-x-2">
-            <Tag
-              color="white"
-              className="border border-t-opp-medium bg-transparent text-t-bright"
-            >
-              Social Media
-            </Tag>
-            <Tag
-              color="white"
-              className="border border-t-opp-medium bg-transparent text-t-bright"
-            >
-              Analytics
-            </Tag>
-          </div>
-          <p className="text-t-bright">
-            Social media content plans, media monitoring, email and text
-            messaging and search engine optimization.
-          </p>
-          <img
-            src="/path/to/marketing-image.png"
-            alt="Marketing Solutions"
-            className="w-full rounded-md"
-          />
-        </FadeInAnimation>
-
-        {/* Brand Identity Card */}
-        <FadeInAnimation
-          index={7}
-          className="h-[620px] space-y-4 rounded-2xl border border-t-opp-medium p-6"
-        >
-          <h3 className="text-2xl font-semibold text-t-bright">
-            Brand identity
-          </h3>
-          <div className="flex space-x-2">
-            <Tag
-              color="white"
-              className="border border-t-opp-medium bg-transparent text-t-bright"
-            >
-              Logo Designs
-            </Tag>
-            <Tag
-              color="white"
-              className="border border-t-opp-medium bg-transparent text-t-bright"
-            >
-              Style Guides
-            </Tag>
-          </div>
-          <p className="text-t-bright">
-            I help my clients to develop a personality and brand voice, design
-            the brand look and logo.
-          </p>
-          <img
-            src="/path/to/brand-image.png"
-            alt="Brand Identity"
-            className="w-full rounded-md"
-          />
-        </FadeInAnimation>
-      </div>
+          Download Resume <Download className="size-4 stroke-[3px]" />
+        </Link>
+      </FadeInAnimation>
     </div>
   );
 }
---
+++ src/components/AwardSection.tsx
@@ -0,0 +1,146 @@
+"use client";
+import React from "react";
+import { useState } from "react";
+import { Button } from "./ui/button";
+
+const international = [
+  {
+    title: "Gold Medal: The Innovation Week in Africa 2021 (IWA 2021)",
+    describe: "bra bra bra",
+    date: "8 - 12 September 2021",
+  },
+  {
+    title: "Bronze Prize: Seoul International Invention Fair 2021 (SIIF 2021)",
+    describe: "bra bra bra",
+    date: "1 -4 December 2021",
+  },
+  {
+    title:
+      "Honorable Mention: World Innovative Science Project Olympiad 2021 (WISPO 2021)",
+    describe: "Sorting Social Media Posts for Help in Disasters",
+    date: "11 - 17 December 2021",
+  },
+  {
+    title: "Bronze Medal: International Youth STEM Tournament 2022 (IYST 2022)",
+    describe:
+      "The Development of Web Application for Check Beta-Agonist in Pork using Deep Learning",
+    date: "28 March 2022",
+  },
+];
+const national = [
+  {
+    title: "Bronze Medel: Thailand Olympiad in Informatics 18 (TOI18)",
+    describe: "bra bra bra",
+    date: "1 - 4 June 2022",
+  },
+  {
+    title:
+      "Certificate from Internationally Outstanding Inventors Award Ceremony 2022 (IOIAC'22)",
+    describe: "bra bra bra",
+    date: "15 June 2022",
+  },
+  {
+    title:
+      "Gold Medal in regional & Honorable Mention: Young Scientist Competition 2022",
+    describe: "bra bra bra",
+    date: "7 March 2022",
+  },
+  {
+    title:
+      "Gold Medal in regional & Honorable Mention: Thailand Young Scientist Festival 18 (TYSF18)",
+    describe: "bra bra bra",
+    date: "18 August 2022",
+  },
+];
+
+function AwardSection() {
+  const [awardsState, setAwardsState] = useState(true);
+  return (
+    <>
+      <h3 className="mt-8 text-3xl font-bold text-t-bright lg:text-4xl">
+        Awards & Achievements
+      </h3>
+      <div className="my-4 space-x-2">
+        <Button
+          className={
+            "rounded-full font-semibold shadow-t-opp-medium transition-transform duration-300 hover:scale-105 " +
+            (awardsState
+              ? "bg-gradient-to-tl from-accent to-secondary text-t-opp-bright"
+              : "text-t-bright")
+          }
+          onClick={(event) => {
+            setAwardsState(true);
+          }}
+        >
+          International
+        </Button>
+        <Button
+          className={
+            "rounded-full font-semibold shadow-t-opp-medium transition-transform duration-300 hover:scale-105 " +
+            (awardsState
+              ? "text-t-bright"
+              : "bg-gradient-to-tl from-accent to-secondary text-t-opp-bright")
+          }
+          onClick={(event) => {
+            setAwardsState(false);
+          }}
+        >
+          National
+        </Button>
+        <div className="py-4">
+          {awardsState
+            ? international.map((item, index) => {
+                return (
+                  <div
+                    key={item.title}
+                    className="flex h-52 space-x-8 rounded-sm p-4 transition-all ease-in hover:cursor-pointer hover:bg-base-tint md:h-32"
+                    style={{ animation: `fadeInAnimation 2s` }}
+                  >
+                    <div className="h-full self-center">
+                      <div className="mask mask-circle relative left-[15%] top-[45%] z-10 h-2 w-2 bg-t-muted"></div>
+                      {index != international.length - 1 ? (
+                        <div className="relative left-[50%] top-[60%] z-10 h-[100%] w-[2px] bg-t-medium"></div>
+                      ) : (
+                        <></>
+                      )}
+                    </div>
+
+                    <div>
+                      <h4 className="text-xl font-semibold">{item.title}</h4>
+                      <time>{item.date}</time>
+                      <p>{item.describe}</p>
+                    </div>
+                  </div>
+                );
+              })
+            : national.map((item, index) => {
+                return (
+                  <div
+                    key={item.title}
+                    className="flex h-52 space-x-8 rounded-sm p-4 transition-all ease-in hover:cursor-pointer hover:bg-base-tint active:bg-base-tint md:h-32"
+                    style={{ animation: `fadeInAnimation 2s` }}
+                  >
+                    <div className="h-full self-center">
+                      <div className="mask mask-circle relative left-[15%] top-[45%] h-2 w-2 bg-t-muted"></div>
+                      {index != national.length - 1 ? (
+                        <div className="relative left-[50%] top-[60%] h-[100%] w-[2px] bg-t-medium"></div>
+                      ) : (
+                        <></>
+                      )}
+                    </div>
+
+                    <div>
+                      <h4 className="text-xl font-semibold">{item.title}</h4>
+                      <p>{item.date}</p>
+                      <p>{item.describe}</p>
+                    </div>
+                  </div>
+                );
+              })}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default AwardSection;
